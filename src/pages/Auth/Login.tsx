f0588dae4fab5ad04f9d11cfbf93a1a3cf99596a
dzkchen <102616038+dzkchen@users.noreply.github.com>
2026-04-16 14:21:30 -0400

    chore: text change

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -130,7 +130,7 @@ const Login = () => {
           <CardHeader className="space-y-2 text-center">
             <CardTitle className="text-2xl font-bold">Welcome</CardTitle>
             <CardDescription>
-              Sign in to access your FraserPay account.
+              Sign in to Access Your FraserPay account.
             </CardDescription>
           </CardHeader>
           
@@ -144,7 +144,7 @@ const Login = () => {
                     <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05" />
                     <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335" />
                   </svg>
-                  Sign in with Google
+                  Sign in With Google
                 </>}
             </Button>
             
@@ -157,7 +157,7 @@ const Login = () => {
             <CardDescription className="w-full text-center pt-4">Running a booth? Fill out the form below.</CardDescription>
             <Button type="button" variant="outline" className="w-full flex items-center justify-center gap-2 h-11" onClick={handleBoothRequestClick} disabled={isGoogleLoading}>
               {isGoogleLoading ? "Signing in..." : <>
-                  <div className="h-full flex place-items-center uppercase rounded-xl bg-gradient-to-r from-purple-500 to-purple-700 p-2 text-white bold">New!</div>Create a booth request
+                  <div className="h-full flex place-items-center uppercase rounded-xl bg-gradient-to-r from-purple-500 to-purple-700 p-2 text-white bold">New!</div>Create a Booth Request
                 </>}
                 {/* Booth Requests Opening Soon... */}
             </Button>
